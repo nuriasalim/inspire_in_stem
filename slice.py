ed81535c3c5c90d5fc70de2e55df8c7dae06657f
nuriasalim <nuriakendi@gmail.com>
2022-05-19 15:10:19 +0300

    lesson on slicing

--- slice.py
+++ slice.py
@@ -1,33 +1,50 @@
+#slicing method
+city = "Nairobi"
+#print(city[:5]) -> slices the word from the front
+#print(city[2:]) -> slices the word from back
+
+
+f_name= "nuria salim"
+#change to upper case -> .upper() , to lowercase -> .lower()
+print(f_name.upper())
+
 #float -> int  int(x)
 #int -> string str(x)
 
+#concatination -> converting from one data types to another using str(), int(), and float()
+
 number= 6
-print(str(6))
-print(int(6))
-print(float(8))
+#print(str(6))
+#print(int(6))
+#print(float(8))
 
 f_name = "James"
 s_name = "Corden"
 
 full_name = f_name + s_name
-print(full_name)
+#print(full_name)
 
 
 #the replace () method replaces existing character in a string with another 
 
 name = "Brett Cooper"
-print(name.replace('t' , 'G'))
+#print(name.replace('t' , 'G'))
 
 #The split() method splits the sentences into single words hence a list
 msg = "Hello there I am Nuria from Nairobi"
 
-print(msg.split())
-
-#The len methodis used in getting the length of a string
-
-print(len(msg))
+#print(msg.split())
 
 
+#The len method is used in getting the length of a string
+#print(len(msg))
 
+
+msg = ''' I am Nuria Mohamed
+ I come from Nairobi
+I am learning how to code
+           '''
+
+print(msg)
 
 
