2a602bd33e540ee36173b86b29e8f1fc474aa577
nuriasalim <nuriakendi@gmail.com>
2022-05-19 10:00:39 +0300

    lesson 6 on lists

---
+++ lesson6.py
@@ -0,0 +1,36 @@
+#lists -> should have box brackets
+
+motorcycle = ['honda' , 'yamaha' , 'suzuki']
+plate_number = ('H1234' , 'Y1234' , 'S1234')
+#print(motorcycle)
+
+#accessing list items using the index
+#print(motorcycle[2])
+
+#print(motorcyvle[3]) -> cannot shot this since its not within the range
+
+#motorcycle[0]= "Bugatti"-> replacing items in the list -> index 0 is the first elemt-. honda
+
+#print(motorcycle)
+#the above replaces index 1 with bugatti
+#motorcycle[0]= "Bugatti"
+#print(motorcycle)
+
+#print("i love"+  str(motorcycle[1]))
+
+#adding element in a list
+#motorcycle.append('Bugatti') 
+#using .append-> adds the item you wwant to the list
+#print(motorcycle)
+
+#print each morocycle and their plate number
+
+#print(motorcycle[1],plate_number[1])
+#print(motorcycle [0],plate_number[0], motorcycle[1],plate_number[1])
+print("i love"  +  str(motorcycle[0]), plate_number[0])
+
+
+
+
+
+
